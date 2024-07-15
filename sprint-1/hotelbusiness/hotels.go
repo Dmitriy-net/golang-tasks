104cb4ad0a1ac530f1c197e715770a2272614d00
Dims-net <mr.babkind@yandex.ru>
2024-07-15 13:08:59 +0300

    init

--- sprint-1/hotelbusiness/hotels.go
+++ sprint-1/hotelbusiness/hotels.go
@@ -2,6 +2,10 @@
 
 package hotelbusiness
 
+import (
+	"sort"
+)
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -13,5 +17,28 @@ type Load struct {
 }
 
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	changes := make(map[int]int)
+
+	for _, guest := range guests {
+		changes[guest.CheckInDate] += 1
+		changes[guest.CheckOutDate] -= 1
+	}
+
+	var dates []int
+	for date := range changes {
+		dates = append(dates, date)
+	}
+	sort.Ints(dates)
+
+	var loads []Load
+	currentGuests := 0
+
+	for _, date := range dates {
+		currentGuests += changes[date]
+		if len(loads) == 0 || loads[len(loads)-1].GuestCount != currentGuests {
+			loads = append(loads, Load{StartDate: date, GuestCount: currentGuests})
+		}
+	}
+
+	return loads
 }
